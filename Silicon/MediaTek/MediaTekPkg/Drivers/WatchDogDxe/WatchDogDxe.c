02a895cf9ba9900e7a9eb374c4b693b5e0184347
Arseniy Velikanov <me@adomerle.pw>
2025-06-08 02:49:13 +0400

    WatchDogDxe: Implement proper timer

--- Silicon/MediaTek/MediaTekPkg/Drivers/WatchDogDxe/WatchDogDxe.c
+++ Silicon/MediaTek/MediaTekPkg/Drivers/WatchDogDxe/WatchDogDxe.c
@@ -4,35 +4,23 @@
 #include <Library/UefiBootServicesTableLib.h>
 #include <Library/DebugLib.h>
 #include <Library/TimerLib.h>
+#include <Library/PcdLib.h>
 
-#define WATCHDOG_BASE (0x10007000)
 #define WATCHDOG_LENGTH (0x4)
 #define WATCHDOG_RESTART (0x8)
 
 #define WDT_LENGTH_KEY (0x8)
 #define WDT_RST_KEY (0x1971)
-#define WDT_MODE_KEY (0x22000000)
 
+UINT32 WatchdogBase;
+
+EFI_EVENT WatchdogTimerEvent;
 EFI_EVENT mEfiExitBootServicesEvent;
-
-UINT32 WatchdogModeBackup;
 
 VOID
 WatchdogPing ()
 {
-  MmioWrite32 (WATCHDOG_BASE + WATCHDOG_RESTART, WDT_RST_KEY);
-}
-
-VOID
-WatchdogDisable ()
-{
-  MmioWrite32 (WATCHDOG_BASE, WDT_MODE_KEY);
-}
-
-VOID
-WatchdogEnable ()
-{
-  MmioWrite32 (WATCHDOG_BASE, WatchdogModeBackup | WDT_MODE_KEY);
+  MmioWrite32 (WatchdogBase + WATCHDOG_RESTART, WDT_RST_KEY);
 }
 
 VOID
@@ -40,18 +28,19 @@ SetTimeout (UINT32 TimeoutMicroSeconds)
 {
   // Watchdog timer timeout period is multiple of 15.6ms
   UINT32 Reg = (TimeoutMicroSeconds / 15600) | WDT_LENGTH_KEY;
-  MmioWrite32 (WATCHDOG_BASE + WATCHDOG_LENGTH, Reg);
+  MmioWrite32 (WatchdogBase + WATCHDOG_LENGTH, Reg);
 
   WatchdogPing ();
 }
 
 VOID
-WatchdogExitBootServicesEvent (
+EFIAPI
+PingWatchdogEvent (
   IN EFI_EVENT Event,
   IN VOID *Context
   )
 {
-  WatchdogEnable ();
+  WatchdogPing ();
 }
 
 EFI_STATUS
@@ -63,18 +52,41 @@ WatchdogDxeEntry (
 {
   EFI_STATUS Status;
 
-  WatchdogModeBackup = MmioRead32 (WATCHDOG_BASE);
-
-  WatchdogDisable ();
+  WatchdogBase = FixedPcdGet32(PcdWatchdogBaseAddress);
 
   // Set watchdog timeout to 30s
   SetTimeout (30*1000*1000);
 
-   // Enable watchdog before os boot
+  Status = gBS->CreateEvent (
+    EVT_TIMER | EVT_NOTIFY_SIGNAL,
+    TPL_CALLBACK,
+    PingWatchdogEvent,
+    NULL,
+    &WatchdogTimerEvent
+  );
+
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR, "WatchDogDxe: Failed to create timer event: %r\n", Status));
+    return Status;
+  }
+
+  // Ping watchdog every 5s
+  Status = gBS->SetTimer (
+    WatchdogTimerEvent,
+    TimerPeriodic,
+    5*1000*1000*10
+  );
+
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR, "WatchDogDxe: Failed to set periodic timer: %r\n", Status));
+    return Status;
+  }
+
+  // Ping watchdog before os boot
   Status = gBS->CreateEvent (
     EVT_SIGNAL_EXIT_BOOT_SERVICES,
     TPL_NOTIFY,
-    WatchdogExitBootServicesEvent,
+    PingWatchdogEvent,
     NULL,
     &mEfiExitBootServicesEvent
   );
@@ -84,5 +96,6 @@ WatchdogDxeEntry (
     return Status;
   }
 
+  DEBUG ((DEBUG_INFO, "WatchDogDxe: Watchdog kicker initialized.\n"));
   return EFI_SUCCESS;
 }
